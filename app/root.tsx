19eb36eca7bc0072f83f990932fd40abaf1c81fa
kkoruev <k.koruev27@gmail.com>
2024-02-20 23:02:59 +0200

    Add test integration with FB and simple homepage action to trigger it.

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,27 @@ import {ThemeProvider} from "@mui/material";
 
 import globalCss from '~/styles/global.css';
 
+function createFacebookSDKScript() {
+   return `
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '1167415367956511',
+        cookie: true,
+        xfbml: true,
+        version: 'v10.0'
+      });
+      FB.AppEvents.logPageView();   
+    };
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  `;
+}
+
 export default function App() {
    return (
       <html lang="en">
@@ -16,6 +37,7 @@ export default function App() {
          />
          <title>Remix: So great, it's funny!</title>
          <Links />
+         <script dangerouslySetInnerHTML={{ __html: createFacebookSDKScript() }} />
       </head>
       <body>
       <ThemeProvider theme={theme}>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,56 @@
-export default function IndexRoute() {
-   return <div>This is cool!</div>;
+import {Box, Button, Container, Typography} from "@mui/material";
+import {Facebook} from "@mui/icons-material";
+import {useState} from "react";
+
+
+export default function HomePage() {
+   const [userName, setUserName] = useState('');
+
+   const handleLogin = () => {
+      if (FB) {
+         FB.login(function(response) {
+            if (response.authResponse) {
+               console.log('Welcome! Fetching your information.... ');
+               FB.api('/me', {fields: 'name,email,birthday,gender,picture'}, function(response) {
+                  console.log('Good to see you, ' + response.name + '.');
+                  console.log(response.picture.data.url);
+                  setUserName(response.name); // Set the user name in state
+               });
+            } else {
+               console.log('User cancelled login or did not fully authorize.');
+            }
+         }, {scope: 'public_profile,email'});
+      }
+   };
+
+   return (
+      <Container maxWidth="sm">
+         <Box
+            sx={{
+               display: 'flex',
+               flexDirection: 'column',
+               alignItems: 'center',
+               justifyContent: 'center',
+               minHeight: '100vh',
+            }}
+         >
+            {!userName && <Typography variant="h4" component="h1" gutterBottom>
+               Welcome to Our Community
+            </Typography>}
+            {userName && <Typography variant="h6">Welcome, {userName}</Typography>} {/* Display the user's name */}
+            {!userName && <Typography variant="body1" paragraph>
+               Join us and let's play football together. Log in to create or join events!
+            </Typography>}
+            {!userName && <Button
+               startIcon={<Facebook />}
+               variant="contained"
+               color="primary"
+               onClick={handleLogin}
+               sx={{ textTransform: 'none' }}
+            >
+               Login with Facebook
+            </Button> }
+         </Box>
+      </Container>
+   );
 }
