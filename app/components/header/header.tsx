4f462983bc9b8661857cefec438bd9cccb0e2e72
kkoruev <k.koruev27@gmail.com>
2024-03-20 10:58:52 +0200

    Add first version of mobile menu navigation.

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,24 +1,59 @@
 import {Link} from "@remix-run/react";
-import {AppBar, Button, IconButton, Toolbar, Typography} from "@mui/material";
-import {Menu} from "@mui/icons-material";
+import {AppBar, Button, IconButton, Menu, MenuItem, Toolbar, Typography} from "@mui/material";
+import {Menu as MenuIcon} from "@mui/icons-material";
 
 import styles from './header.css';
+import {useState} from "react";
 
 export default function Header() {
+
+   const [anchorEl, setAnchorEl] = useState(null);
+   const open = Boolean(anchorEl);
+
+   const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+   };
+
+   const handleClose = () => {
+      setAnchorEl(null);
+   };
 
    return (
       <header className="header">
          <AppBar position="static" color="primary" elevation={0}>
             <Toolbar>
                <div className="mobileOnly">
-                  <IconButton edge="start" color="inherit" aria-label="menu" sx={{mr: 2}}>
-                     <Menu/>
+                  <IconButton edge="start" color="inherit" aria-label="menu" sx={{mr: 2}} onClick={handleMenu}>
+                     <MenuIcon/>
                   </IconButton>
+                  <Menu
+                     id="menu-appbar"
+                     anchorEl={anchorEl}
+                     anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                     }}
+                     keepMounted
+                     transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                     }}
+                     open={open}
+                     onClose={handleClose}
+                     MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                     }}
+                  >
+                     <MenuItem onClick={handleClose} component={Link} to="/invitations" >Events</MenuItem>
+                     <MenuItem onClick={handleClose} component={Link} to="/invitations/new" >Create Event</MenuItem>
+                     {/* Add more menu items as needed */}
+                  </Menu>
                </div>
 
                <Typography variant="h6" color="inherit" noWrap sx={{flexGrow: 1}}>
                   SportsHub
                </Typography>
+
 
                <div className="desktopOnly">
                   <Button color="inherit" component={Link} to="/invitations">Events</Button>
