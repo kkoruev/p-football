99c49e438d5abe21726142c4d72c6a5402c85b60
kkoruev <k.koruev27@gmail.com>
2024-03-27 18:32:28 +0200

    Add initial db integration to Event creaiton.

---
+++ app/data/invitation/create.invitation.db.ts
@@ -0,0 +1,6 @@
+import {BaseInvitation} from "~/data/invitation/base.invitation";
+import {CreateInvitation} from "~/data/invitation/create.invitation";
+
+export interface CreateInvitationDb extends CreateInvitation {
+   dateTime: Date;
+}
